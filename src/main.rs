cb3079b2d7ed9b441845b6009c9d964ab2a68440
Simon Goller <neosam+github@posteo.de>
2023-12-07 19:35:57 +0100

    Moving ship with stars

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@
 
 use bevy::{asset::AssetMetaCheck, input::mouse::MouseMotion, prelude::*, window::CursorGrabMode};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy_rapier3d::prelude::*;
+use rand::prelude::*;
 
 #[derive(Component, Reflect)]
 pub struct ActiveCamera {
@@ -42,23 +44,47 @@ impl Default for CameraControllerState {
 #[derive(Component)]
 pub struct CameraFocus;
 
+#[derive(Component)]
+pub struct Star;
+
+#[derive(Component)]
+pub struct Player;
+#[derive(Component)]
+pub struct Ship {
+    pub speed: f32,
+}
+
+#[derive(Bundle)]
+pub struct StarBundle {
+    pub pbr_bundle: PbrBundle,
+    pub star: Star,
+}
+
 fn main() {
     let mut app = App::new();
     app.insert_resource(AssetMetaCheck::Never)
         .add_plugins(DefaultPlugins)
         .register_type::<ActiveCamera>()
-        .register_type::<CameraControllerState>();
+        .register_type::<CameraControllerState>()
+        .insert_resource(RapierConfiguration {
+            gravity: Vec3::new(0.0, 0.0, 0.0),
+            ..Default::default()
+        })
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugins(RapierDebugRenderPlugin::default());
 
     if cfg!(debug_assertions) {
         app.add_plugins(WorldInspectorPlugin::new());
     }
 
-    app.add_systems(Startup, setup)
+    app.add_systems(Startup, (setup, setup_stars))
         .add_systems(
             Update,
             (
                 camera_transform_update,
                 camera_controller.pipe(error_handler),
+                player_velocity_controller.pipe(error_handler),
+                respawn_stars.pipe(error_handler),
             ),
         )
         .run();
@@ -82,13 +108,81 @@ pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         },
         Name::new("Ship"),
         CameraFocus,
+        Ship { speed: 10.0 },
+        Player,
+        Collider::cuboid(1.0, 0.4, 1.0),
+        RigidBody::Dynamic,
+        Velocity::default(),
     ));
     commands.insert_resource(AmbientLight {
         color: Color::ALICE_BLUE,
         brightness: 0.8,
     });
+
     commands.insert_resource(ClearColor(Color::BLACK));
     commands.insert_resource(CameraControllerState::default());
+}
+
+pub fn setup_stars(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    let star_material = materials.add(StandardMaterial {
+        base_color: Color::WHITE,
+        unlit: true,
+        ..Default::default()
+    });
+    let star_shape = meshes.add(
+        shape::UVSphere {
+            radius: 0.3,
+            ..Default::default()
+        }
+        .into(),
+    );
+    commands.spawn_batch((0..1000).map(move |_| {
+        let mut rng = rand::thread_rng();
+        let phi = rng.gen_range(0.0..std::f32::consts::PI * 2.0);
+        let theta = rng.gen_range(0.0..std::f32::consts::PI * 2.0);
+        let distance = rng.gen_range(20.0..100.0);
+        let x = distance * phi.sin() * theta.cos();
+        let y = distance * phi.sin() * theta.sin();
+        let z = distance * phi.cos();
+        let mut transform = Transform::from_translation(Vec3::new(x, y, z));
+        transform.scale = Vec3::splat(0.1);
+        StarBundle {
+            pbr_bundle: PbrBundle {
+                mesh: star_shape.clone(),
+                material: star_material.clone(),
+                transform,
+                ..Default::default()
+            },
+            star: Star,
+        }
+    }));
+}
+
+pub fn respawn_stars(
+    mut star_query: Query<&mut Transform, (With<Star>, Without<ActiveCamera>)>,
+    camera_query: Query<&Transform, With<ActiveCamera>>,
+) -> anyhow::Result<()> {
+    let camera_transform = camera_query.get_single()?;
+    for mut star_transform in star_query.iter_mut() {
+        if star_transform
+            .translation
+            .distance(camera_transform.translation)
+            > 100.0
+        {
+            //let mut rng = rand::thread_rng();
+            //let x = rng.gen_range(-100.0..100.0);
+            //let y = rng.gen_range(-100.0..100.0);
+            //let z = rng.gen_range(-100.0..100.0);
+            let diff = star_transform.translation - camera_transform.translation;
+            star_transform.translation = camera_transform.translation - diff;
+        }
+    }
+
+    Ok(())
 }
 
 pub fn camera_transform_update(
@@ -140,6 +234,15 @@ pub fn camera_controller(
             }
         }
     }
+
+    Ok(())
+}
+
+pub fn player_velocity_controller(
+    mut player_query: Query<(&mut Velocity, &Transform, &Ship), With<Player>>,
+) -> anyhow::Result<()> {
+    let (mut velocity, transform, ship) = player_query.get_single_mut()?;
+    velocity.linvel = transform.rotation * Vec3::new(0.0, 0.0, ship.speed);
 
     Ok(())
 }
